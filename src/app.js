1c2c2a547d3d3a8291f41a65148004260aca8083
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-20 23:28:39 -0600

    Finish add number function

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 class Calculator {
 	constructor() {
+		this.firstNumber = [];
+		this.secondNumber = [];
+
+		this.$screen = document.getElementById('screen');
+		this.$calculator = document.querySelector('.calculator');
+
 		this.$zero = document.getElementById('zero');
 		this.$one = document.getElementById('one');
 		this.$two = document.getElementById('two');
@@ -11,12 +17,51 @@ class Calculator {
 		this.$eight = document.getElementById('eight');
 		this.$nine = document.getElementById('nine');
 
-		this.$multi = document.getElementById('multi')
-		this.$divide = document.getElementById('divide')
-		this.$sum = document.getElementById('sum')
-		this.$minus = document.getElementById('minus')
-		this.$c = document.getElementById('c')
-		this.$equal = document.getElementById('equal')
+		this.$multi = document.getElementById('multi');
+		this.$divide = document.getElementById('divide');
+		this.$sum = document.getElementById('sum');
+		this.$minus = document.getElementById('minus');
+		this.$c = document.getElementById('c');
+		this.$equal = document.getElementById('equal');
+
+		this.addEventListeners();
+	}
+
+	addEventListeners() {
+		this.$calculator.addEventListener('click', (event) => {
+			this.pressNumber(event);
+		});
+	}
+
+	pressNumber(event) {
+		event.stopPropagation();
+
+		if (event.target.matches('#one')) {
+			this.addNumbersInsideFirstNumber(1);
+		} else if (event.target.matches('#two')) {
+			this.addNumbersInsideFirstNumber(2);
+		} else if (event.target.matches('#three')) {
+			this.addNumbersInsideFirstNumber(3);
+		} else if (event.target.matches('#four')) {
+			this.addNumbersInsideFirstNumber(4);
+		} else if (event.target.matches('#five')) {
+			this.addNumbersInsideFirstNumber(5);
+		} else if (event.target.matches('#six')) {
+			this.addNumbersInsideFirstNumber(6);
+		} else if (event.target.matches('#seven')) {
+			this.addNumbersInsideFirstNumber(7);
+		} else if (event.target.matches('#eight')) {
+			this.addNumbersInsideFirstNumber(8);
+		} else if (event.target.matches('#nine')) {
+			this.addNumbersInsideFirstNumber(9);
+		} else if (event.target.matches('#zero')) {
+			this.addNumbersInsideFirstNumber(0);
+		}
+	}
+
+	addNumbersInsideFirstNumber(number) {
+		this.firstNumber.push(number);
+		this.$screen.innerText = this.firstNumber.join('');
 	}
 }
 
